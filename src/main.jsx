63b303d4b11ee0ee04c13d57899a64e2e661ee36
icesplashyt-dotcom <icesplashyt@icloud.com>
2026-09-05 11:04:41 +0100

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import RMBPayDashboard from "./RMBPayDashboard.jsx";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RMBPayDashboard />
+  </React.StrictMode>
+);
